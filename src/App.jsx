a53a8c578420c23fa1311e8ff718bebe3696958a
AnhTuTran12 <tutrananh.266@gmail.com>
2025-06-04 23:15:07 +0700

    clear

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Home";
@@ -7,8 +6,6 @@ import Footer from "./components/Footer";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Header />
