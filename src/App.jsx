a27d62035aa5957b0d588e4a15f8afa2788b5175
AnhTuTran12 <tutrananh.266@gmail.com>
2025-06-04 23:12:32 +0700

    add router in file app

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,7 +12,11 @@ function App() {
   return (
     <>
       <Header />
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </BrowserRouter>
       <Footer />
     </>
   );
